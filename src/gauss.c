ca28fea7b44834a70c62d69f5b3a2576cb9dada1
MichaelL200 <michal.ludwiczak.12345@gmail.com>
2024-12-21 14:08:12 +0100

    get_main_elem and switch_rows added to gauss

--- src/gauss.c
+++ src/gauss.c
@@ -2,6 +2,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// zwraca główny element dla danej kolumny macierzy
+int get_main_elem(Matrix* mat, int col)
+{
+	double main_elem_val = abs(mat->data[0][col]);
+	int main_elem = 0;
+
+	for(int i = 0; i < mat->r; i++)
+		if(abs(mat->data[i][col]) > main_elem_val)
+		{
+			main_elem_val = abs(mat->data[i][col]);
+			main_elem = i;
+		}
+
+	return main_elem;
+}
+
+// zamienia wiersze tak, by główny element był na diagonalu
+void switch_rows(Matrix* mat, Matrix* b, int main_elem, int col)
+{
+	if(main_elem != col)
+	{
+		double tmp;
+		for(int i = col; i < mat->c; i++)
+		{
+			tmp = mat->data[0][i];
+			mat->data[0][i] = mat->data[main_elem][i];
+			mat->data[main_elem][i] = tmp; 
+		}
+		tmp = b->data[0][0];
+		b->data[0][0] = b->data[main_elem][0];
+		b->data[main_elem][0] = tmp;
+	}
+}
+
 // zmienia wartości wierszy dla każdego miejsca do wyzerowania
 void substract_rows(Matrix* mat, Matrix* b, int d)
 {
@@ -27,6 +61,12 @@ int eliminate(Matrix *mat, Matrix *b)
 	// dla każdej kolumny (indeksów diagonala)
     for(int d = 0; d < mat->r; d++)
 	{
+		// zwraca główny element
+		int main_elem = get_main_elem(mat, d);
+
+		// zamienia wiersze
+		switch_rows(mat, b, main_elem, d);
+
 		// obsłuża przypadek dla zera i zwraca 1
 		if(mat->data[d][d] == 0)
 			return 1;
--- src/gauss.h
+++ src/gauss.h
@@ -3,6 +3,12 @@
 
 #include "mat_io.h"
 
+// zwraca główny element dla danej kolumny macierzy
+int get_main_elem(Matrix* mat, int col);
+
+// zamienia wiersze tak, by główny element był na diagonalu
+void switch_rows(Matrix* mat, Matrix* b, int main_elem, int col);
+
 // zmienia wartości wierszy dla każdego miejsca do wyzerowania
 void substract_rows(Matrix* mat, Matrix* b, int d);
 
